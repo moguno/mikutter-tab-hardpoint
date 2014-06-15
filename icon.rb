d1bdd3ad076988d2bbc9bda08804f4bd96c2af4b
moguno <moguno@0kn.sakura.ne.jp>
2014-06-16 01:01:11 +0900

    タブがサイドにある場合は、ラベルウィジェットを縦に配置する様にした。

--- icon.rb
+++ icon.rb
@@ -8,21 +8,14 @@ def define_icon_proc(plugin)
 
     on_boot do
       UserConfig.connect(:tab_icon_size) { |key, val, before, id|
-        i_tabs.each { |i_tab|
-          icon = i_tab.icon
-          i_tab.set_icon nil
-          i_tab.set_icon icon
+        Plugin::GUI::Tab.cuscaded.each { |slug, i_tab|
+          Plugin[:mikutter_tab_extension].tab_update_widgets(i_tab)
         }
       }
     end
 
     settings "タブのアイコンサイズ" do
       adjustment("アイコンサイズ", :tab_icon_size, 1, 100)
-    end
-
-    on_tab_created do |i_tab|
-      i_tabs << i_tab
-      i_tab.set_icon i_tab.icon
     end
 
     filter_tab_update_widget { |i_tab, widgets|
--- mikutter-tab-extension.rb
+++ mikutter-tab-extension.rb
@@ -3,6 +3,23 @@
 require File.join(File.dirname(__FILE__), "icon.rb")
 
 Plugin.create(:mikutter_tab_extension) {
+  TabInfo = Struct.new(:container, :angle)
+
+  @tab_infos = [
+    TabInfo.new(::Gtk::HBox, 0),
+    TabInfo.new(::Gtk::HBox, 0),
+    TabInfo.new(::Gtk::VBox, 90),
+    TabInfo.new(::Gtk::VBox, 270),
+  ]
+
+  on_boot { |service|
+    UserConfig.connect(:tab_position) { |key, val, before, id|
+      Plugin::GUI::Tab.cuscaded.each { |slug, i_tab|
+        Plugin[:mikutter_tab_extension].tab_update_widgets(i_tab)
+      }
+     }
+  }
+
   Plugin[:gtk].instance_eval {
     alias :tab_update_icon_moguno :tab_update_icon
 
@@ -12,25 +29,31 @@ Plugin.create(:mikutter_tab_extension) {
   }
 
   def tab_update_widgets(i_tab)
-      type_strict i_tab => Plugin::GUI::TabLike
+    type_strict i_tab => Plugin::GUI::TabLike
 
-      tab = Plugin[:gtk].widgetof(i_tab)
+    tab = Plugin[:gtk].widgetof(i_tab)
 
-      if tab
-        tab.tooltip(i_tab.name)
-        tab.remove(tab.child) if tab.child
+    if tab
+      tab.tooltip(i_tab.name)
+      tab.remove(tab.child) if tab.child
 
-        widgets = Plugin.filtering(:tab_update_widget, i_tab, {:left => [], :center => [], :right => []})
+      widgets = Plugin.filtering(:tab_update_widget, i_tab, {:left => [], :center => [], :right => []})
 
-        box = ::Gtk::HBox.new
+      Delayer.new {
+        box = @tab_infos[UserConfig[:tab_position]].container.new
 
         [:left, :center, :right].each { |key|
           widgets[1][key].each { |widget|
+            if widget.is_a?(::Gtk::Label)
+              widget.angle = @tab_infos[UserConfig[:tab_position]].angle
+            end
+
             box.pack_start(widget)
           }
         }
 
         tab.add(box).show_all
+      }
     end
   end
 
