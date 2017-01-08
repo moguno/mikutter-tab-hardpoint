280337309594bcd369eb4d4284d9f69af872a64e
moguno <moguno@0kn.sakura.ne.jp>
2017-01-08 21:37:58 +0900

    mikutter 3.5以前の対応が怪しかったのを修正

--- icon.rb
+++ icon.rb
@@ -19,7 +19,17 @@ def define_icon_proc(plugin)
     end
 
     filter_tab_update_widget { |i_tab, widgets|
-      widget = if i_tab.icon.is_a?(String) || i_tab.icon.class.include?(Retriever::Model::PhotoMixin)
+
+      # mikutter 3.5のRetriever::Model::PhotoMixinが存在する場合は、それがインクルードされているか確認
+      is_icon = if defined?(Retriever::Model::PhotoMixin)
+        i_tab.icon.class.include?(Retriever::Model::PhotoMixin)
+
+      # そうじゃ無い場合は、Stringクラスか確認
+      else
+        i_tab.icon.is_a?(String)
+      end
+
+      widget = if is_icon
         ::Gtk::WebIcon.new(i_tab.icon, UserConfig[:tab_icon_size], UserConfig[:tab_icon_size])
       else
         ::Gtk::Label.new(i_tab.name)
